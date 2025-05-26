32fcb040adc7ae5328588b3652c1bb3a3e19f3b9
JoaquinHC9 <joaquin.hidalgo@unmsm.edu.pe>
2025-05-25 23:07:55 -0500

    first commit

---
+++ safechat-core/src/main/java/com/pe/fisi/sw/safechat_core/SafechatCoreApplication.java
@@ -0,0 +1,13 @@
+package com.pe.fisi.sw.safechat_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafechatCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafechatCoreApplication.class, args);
+	}
+
+}
---
+++ safechat-core/src/test/java/com/pe/fisi/sw/safechat_core/SafechatCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.fisi.sw.safechat_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafechatCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
